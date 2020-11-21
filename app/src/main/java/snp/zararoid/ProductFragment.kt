a9d459d87d80520d3adc9029ddf1bd3c69e1685d
yjooooo <thsduswn4141@naver.com>
2020-11-21 20:40:45 +0900

    TabLayout에서 item 글씨체 적용 선택됨 : inter_bold 선택안됨 : inter_regular

--- app/src/main/java/snp/zararoid/ProductFragment.kt
+++ app/src/main/java/snp/zararoid/ProductFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.FontRes
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.get
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_product.*
 import snp.zararoid.rcv.ProductAdapter
 import snp.zararoid.rcv.ProductData
@@ -58,6 +64,36 @@ class ProductFragment : Fragment() {
                 ProductData(0, R.drawable.img, 2, "CONTRAST PUFFER JACKET", 0,99000, false)
         )
 
+
+        //카테고리 탭 아이템 글씨체 selected : inter_bold, unselected : inter_regular
+        fun changeSelectedTabItemFontFamily(tabPosition: Int, @FontRes fontFamilyRes: Int) {
+            val linearLayout = (tab_product.getChildAt(0) as ViewGroup).getChildAt(tabPosition) as LinearLayout
+            val tabTextView = linearLayout.getChildAt(1) as TextView
+            val typeface = ResourcesCompat.getFont(view.context, fontFamilyRes)
+            tabTextView.typeface = typeface
+        }
+
+        
+        //탭아이템에서 디폴트로 bold글씨체는 첫번째 아이템
+        changeSelectedTabItemFontFamily(0, R.font.inter_bold)
+
+        tab_product.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.position?.let {
+                    changeSelectedTabItemFontFamily(it, R.font.inter_regular)
+                }
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.position?.let {
+                    changeSelectedTabItemFontFamily(it, R.font.inter_bold)
+                }
+
+            }
+
+        })
 
 
     }
